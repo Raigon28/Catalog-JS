4dc902e7bab2bbf7cf156138de773fff37f34db9
Andrey <raigon28@gmail.com>
2021-06-12 14:57:02 +0300

    Added json API

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function render() {
 
 let CATALOG = [];
 
+//You can also use 'https://sleepy-cliffs-19222.herokuapp.com/3sAo6GF0v.json'
+
 fetch('server/catalog.json')
     .then(res => res.json())
     .then(body => {
